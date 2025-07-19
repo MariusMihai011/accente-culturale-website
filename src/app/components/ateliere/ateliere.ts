d3fcbed853475b84edbd9a11626aaa6a517a2cc4
MariusMihai011 <mariusmihai.miron@gmail.com>
2025-07-19 18:02:30 +0300

    separat site pe componente

---
+++ src/app/components/ateliere/ateliere.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ateliere',
+  imports: [],
+  templateUrl: './ateliere.html',
+  styleUrl: './ateliere.css'
+})
+export class Ateliere {
+
+}
