ea7cfb8d50ca5066db9fd893072b26fc104f169d
MariusMihai011 <mariusmihai.miron@gmail.com>
2025-07-20 19:54:16 +0300

    routing done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,34 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-//   protected title = 'Accente Culturale';
-//   email = "accenteculturalecristian@gmail.com"
-//   phone = "0721246424"
-//   address = "Strada Ghimbavului 40A, Cristian, Brașov"
-//   highlightedSection: string | null = null;
 
-// highlightSection(id: string) {
-//   // Scroll smoothly to the section
-//   const el = document.getElementById(id);
-//   if (el) {
-//     el.scrollIntoView({ behavior: 'smooth' });
-//   }
-
-//   // Highlight the section title (the <h1> inside the section)
-//   this.highlightedSection = id;
-
-//   // Remove highlight after 1 second
-//   setTimeout(() => {
-//     this.highlightedSection = null;
-//   }, 1000);
-// }
   contactInfo = {
     phone: '+40 721 246 424', // Replace with actual phone
     email: 'accenteculturalecristian@gmail.com', // Replace with actual email
